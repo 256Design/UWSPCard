0beac7253d4f92d3920edf6b7d0196390b52930f
Spencer Oberstadt <soberstadt@gmail.com>
2012-04-21 12:48:50 -0500

    Add basic login

---
+++ src/com/twofivesix/uwspCard/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.twofivesix.uwspCard.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.twofivesix.uwspCard.R;
+
+public class LoginActivity extends Activity
+{
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
